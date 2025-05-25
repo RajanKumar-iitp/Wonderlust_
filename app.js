9dffb8bd40ed24d3b3d4f8a2a19af37dfd24a797
Rajan Kumar <164497147+RajanKumar-iitp@users.noreply.github.com>
2025-05-26 03:08:06 +0530

    Update app.js
    Phase Part b

--- app.js
+++ app.js
@@ -1,94 +1,107 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
+const Listing = require("./models/listing.js")//listing model
 const path = require("path");
-const methodOverride = require("method-override");
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');//ejsMate
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));//update
+app.engine('ejs', ejsMate); //ejsMATE
+app.use(express.static(path.join(__dirname, "public")));//css
+
 
 app.get("/", (req, res) => {
-  res.send("Hi, I am root");
+    res.send("Hi, I am root");
 });
 
-//Index Route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
+//index route                              
+app.get("/listings", async (req, res) => { 
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
 });
 
-//New Route
+//New Route                            // write above id route
 app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
+    res.render("listings/new.ejs");
 });
 
 //Show Route
 app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
 });
 
 //Create Route
 app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
+    // let {title, description, image, price, country, location} = req.body;
+    // let listing = req.body.listing;
+    // console.log(listing);
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();//Directly save data in document
+    res.redirect("/listings");
 });
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
 });
+
+
+//Update Route
+// const methodOverride = require('method-override');
+// app.use(methodOverride('_method'));
 
 //Update Route
 app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect("/listings");
 });
 
 //Delete Route
 app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 });
 
+// //listing(1) model
 // app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My New Villa",
-//     description: "By the beach",
-//     price: 1200,
-//     location: "Calangute, Goa",
-//     country: "India",
-//   });
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
 
-//   await sampleListing.save();
-//   console.log("sample was saved");
-//   res.send("successful testing");
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
 // });
 
 app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+    console.log("server is listening to port 8080");
 });
